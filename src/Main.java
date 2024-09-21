87d61416492d9e1572c05db38904e5a0a1f1a48c
Carmelo Scribano <56937452+CarmeloScribano@users.noreply.github.com>
2024-09-21 00:44:59 -0400

    Fixed the trailing zeros again

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import views.Formatter;
 public class Main {
     public static void main(String args[]) throws IOException {
         long startTime = System.currentTimeMillis();
-        String formattedOutput = new Formatter().printFormat(new Calculator().getStations("./data/measurements-billion.txt"));
+        String formattedOutput = Formatter.printFormat(new Calculator().getStations("./data/measurements-billion.txt"));
         
         System.out.println(formattedOutput);
         System.out.printf("Execution time: %.3f seconds%n", (System.currentTimeMillis() - startTime) / 1000.0);
--- src/views/Formatter.java
+++ src/views/Formatter.java
@@ -5,24 +5,19 @@ import java.util.TreeMap;
 import models.StationValues;
 
 public class Formatter {
-    public String printFormat(TreeMap<String, StationValues> input){
+    public static String printFormat(TreeMap<String, StationValues> input){
         StringBuilder result = new StringBuilder("{");
-        DecimalFormat df = new DecimalFormat("0.##");
+        DecimalFormat df = new DecimalFormat("#.#");
         
         input.forEach((key, value) -> {
             result.append(key).append("=")
                 .append(value.min).append("/")
-                .append(formatMean(value.mean, df)).append("/")
+                .append(df.format(value.mean)).append("/")
                 .append(value.max).append(", ");
         });
         
         result.setLength(result.length() - 2); 
 
         return result + "}";
-    }
-
-    private static String formatMean(double value, DecimalFormat df) {
-        String formattedValue = df.format(Math.round(value * 100.0) / 100.0);
-        return formattedValue.contains(".") ? formattedValue : formattedValue + ".0";
     }
 }
